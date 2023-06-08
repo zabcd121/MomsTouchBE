0b6932b6a2b0a8b06e15fe1b6709ab622ce0be58
zabcd121 <zabcd121@naver.com>
2023-06-08 16:07:02 +0900

    프로젝트 생성

---
+++ src/main/java/com/momstouch/momstouchbe/MomsTouchBeApplication.java
@@ -0,0 +1,13 @@
+package com.momstouch.momstouchbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomsTouchBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomsTouchBeApplication.class, args);
+    }
+
+}
