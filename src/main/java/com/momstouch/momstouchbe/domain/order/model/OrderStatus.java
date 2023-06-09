9e37c59c6cfcf8704f494ea4cbef3d89437b0764
ma-an-jong <alswhd1113@gmail.com>
2023-06-09 13:24:13 +0900

    🦑OrderStatus Accept 상태 추가

--- src/main/java/com/momstouch/momstouchbe/domain/order/model/OrderStatus.java
+++ src/main/java/com/momstouch/momstouchbe/domain/order/model/OrderStatus.java
@@ -2,6 +2,5 @@ package com.momstouch.momstouchbe.domain.order.model;
 
 public enum OrderStatus {
 
-    //TODO : ACCEPT(주문 승낙)
-    ORDER, DELIVERY, COMPLETE, CANCEL
+    ORDER, ACCEPT,DELIVERY, COMPLETE, CANCEL
 }
